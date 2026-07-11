8481eddd1c1271169884d4833e72075af8c15a9f
Teplykh Semyon <teplihsemen@gmail.com>
2026-07-11 23:29:47 +0500

    Наборы символов + вспомогательные функции + реализация init_options

--- lib/src/lib_main.c
+++ lib/src/lib_main.c
@@ -10,7 +10,52 @@ lib_main.c - главный модуль библиотеки.
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#define SET_LOWER "abcdefghijklmnopqrstuvwxyz"
+#define SET_UPPER "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define SET_DIGIT "0123456789"
+#define SET_SPEC  "!@#$%^&*()-_=+[]{}|;:,.<>?"
 
 void init_options(GenOptions* opts) {
-    // заглушка
+    memset(opts, 0, sizeof(GenOptions)); // все флаги = 0
+    opts->count = 1; 
+}
+
+// строка - числа
+static int is_numeric(const char* s) {
+    if (s == NULL || *s == '\0') {
+        return 0; 
+    }
+    while (*s != '\0') {
+        if (*s < '0' || *s > '9') {
+            return 0; 
+        }
+        s++;
+    }
+    return 1;
+}
+
+// смотрим разделитеди
+static int is_sep(char c, const char* seps, int cnt) {
+    for (int i = 0; i < cnt; i++) {
+        if (c == seps[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+// заглушка!!! дописать
+int parse_args(int argc, char** argv, GenOptions* opts) {
+    (void)argc; (void)argv; (void)opts; // параметры пока не надо
+    return 0;
+}
+
+int validate_options(const GenOptions* opts) {
+    (void)opts;
+    return 0;
+}
+
+int generate_passwords(const GenOptions* opts) {
+    (void)opts;
+    printf("There is no generation yet\n");
+    return 0;
 }
